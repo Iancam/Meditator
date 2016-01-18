1066a3caafa7a0c2839b7fd07c682154672018d1
Ian Campbell <iancam@stanford.edu>
2016-01-17 22:04:02 -0700

    Timer updates as it should, and reacts to Start and Stop Button.

--- Meditator/FirstViewController.swift
+++ Meditator/FirstViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+//    MARK: properties
+    
+    @IBOutlet weak var timeDisplay: UILabel!
+    var timer: NSTimer?
+    var count: Int = 0
+
+    //   MARK: actions
+    
+    @IBAction func startStopTime(sender: UIButton) {
+        timeDisplay.text = "0:00:00"
+        if sender.titleForState(.Normal) == "Start"{
+            sender.setTitle("Stop", forState: .Normal)
+            sender.setTitleColor(UIColor.redColor(), forState: .Normal)
+            timer = NSTimer(timeInterval: 1.0, target: self, selector: "countUp", userInfo: nil, repeats: true)
+            NSRunLoop.currentRunLoop().addTimer(timer!, forMode: NSRunLoopCommonModes)
+            
+//            start the timer
+        } else if sender.titleForState(.Normal) == "Stop"{
+            sender.setTitle("Start", forState: .Normal)
+            sender.setTitleColor(UIColor.blueColor(), forState: .Normal)
+            timer?.invalidate()
+            count = 0
+//
+//            log the resulting time
+        }
+        
+    }
+    
+    
+    // MARK: helper functions
+    
+    func countUp(){
+        count++
+        updateTimeLabel()
+    }
+    
+    func updateTimeLabel(){
+        let seconds = String(format: "%02d", count%60)
+        let minutes = String(format: "%02d", (count/60)%60)
+        let hours   = String(format: "%01d", count/(60*60))
+        
+        timeDisplay.text = "\(hours):\(minutes):\(seconds)"
+        
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
